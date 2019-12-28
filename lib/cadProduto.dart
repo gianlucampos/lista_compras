a6fe488b779c0ca617936288bc1d2d0b69735512
gianlucampos <gianlucampos97@gmail.com>
2019-12-28 16:23:48 -0300

    Ajustando posicionamento Habilitando Checkbox Utilizando SingleChildScrollView para evitar overflow

--- lib/cadProduto.dart
+++ lib/cadProduto.dart
@@ -11,97 +11,128 @@ class CadProduto extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                  labelText: 'Nome', hintText: 'Digite o nome do produto'),
-              keyboardType: TextInputType.number,
-              controller: null,
-              onSubmitted: (_) => null,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.45,
-                  child: TextField(
-                    decoration:
-                        InputDecoration(labelText: 'Quantidade', hintText: '1'),
-                    keyboardType: TextInputType.number,
-                    controller: null,
-                    onSubmitted: (_) => null,
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                    labelText: 'Nome', hintText: 'Digite o nome do produto'),
+                keyboardType: TextInputType.number,
+                controller: null,
+                onSubmitted: (_) => null,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width * 0.40,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: 'Quantidade', hintText: '1'),
+                      keyboardType: TextInputType.number,
+                      controller: null,
+                      onSubmitted: (_) => null,
+                    ),
                   ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.45,
-                  child: TextField(
-                    decoration:
-                        InputDecoration(labelText: 'Unidade', hintText: '1'),
-                    keyboardType: TextInputType.number,
-                    controller: null,
-                    onSubmitted: (_) => null,
+                  Container(
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width * 0.48,
+                    child: TextField(
+                      decoration:
+                      InputDecoration(labelText: 'Unidade', hintText: '1'),
+                      keyboardType: TextInputType.number,
+                      controller: null,
+                      onSubmitted: (_) => null,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.45,
-                  child: TextField(
-                    decoration:
-                        InputDecoration(labelText: 'Preço', hintText: '0.00'),
-                    keyboardType: TextInputType.number,
-                    controller: null,
-                    onSubmitted: (_) => null,
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width * 0.40,
+                    child: TextField(
+                      decoration:
+                      InputDecoration(labelText: 'Preço', hintText: '0.00'),
+                      keyboardType: TextInputType.number,
+                      controller: null,
+                      onSubmitted: (_) => null,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
+                  Container(
                     child: Column(
+                      mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
-                        Text('Inserir no Carrinho'),
-                        Container(
-                          color: Colors.orange,
-                          child: Row(
-                            children: <Widget>[
-                              Icon(Icons.add_shopping_cart),
-                              Checkbox(
-                                value: true,
-                                onChanged: null,
-                              ),
-                            ],
+                        Flexible(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 10, left: 10),
+                            child: Text('Inserir no Carrinho'),
                           ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(Icons.add_shopping_cart, size: 30),
+                            CheckBoxWidget(),
+                          ],
                         )
                       ],
                     ),
                   ),
-                ),
-              ],
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Categoria'),
-              keyboardType: TextInputType.number,
-              controller: null,
-              onSubmitted: (_) => null,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Observação'),
-              keyboardType: TextInputType.text,
-              controller: null,
-              onSubmitted: (_) => null,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: 'Foto'),
-              keyboardType: TextInputType.text,
-              controller: null,
-              onSubmitted: (_) => null,
-            ),
-          ],
+                ],
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Categoria'),
+                keyboardType: TextInputType.number,
+                controller: null,
+                onSubmitted: (_) => null,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Observação'),
+                keyboardType: TextInputType.text,
+                controller: null,
+                onSubmitted: (_) => null,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Foto'),
+                keyboardType: TextInputType.text,
+                controller: null,
+                onSubmitted: (_) => null,
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class CheckBoxWidget extends StatefulWidget {
+  @override
+  _CheckBoxWidgetState createState() => _CheckBoxWidgetState();
+}
+
+class _CheckBoxWidgetState extends State<CheckBoxWidget> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      tristate: false,
+      value: isChecked,
+      onChanged: (value) {
+        setState(() {
+          isChecked = value;
+        });
+      },
     );
   }
 }
