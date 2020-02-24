854987c736c3da882b82795e6131d0de34a78b0b
gianlucampos <gianlucampos97@gmail.com>
2020-02-23 22:48:04 -0300

    Otimizando código de multi seleção para listview

--- lib/tests/MultiSelectionList.dart
+++ lib/tests/MultiSelectionList.dart
@@ -3,54 +3,44 @@ import 'package:flutter/scheduler.dart';
 
 class MultiSelection extends StatefulWidget {
   @override
-  _MultiSelectionState createState() => new _MultiSelectionState();
+  _MultiSelectionState createState() => _MultiSelectionState();
 }
 
 class _MultiSelectionState extends State<MultiSelection> {
   bool longPressFlag = false;
-  List<Element> indexList = new List();
-  int selectedCount = 0;
+  List<Element> indexList = [
+    Element(isSelected: false),
+    Element(isSelected: false),
+    Element(isSelected: false)
+  ];
 
   void longPress() {
     setState(() {
-      if (indexList.isEmpty) {
-        longPressFlag = false;
-      } else {
-        longPressFlag = true;
-      }
+      longPressFlag = indexList.isNotEmpty;
+    });
+  }
+
+  void onElementSelected(int index) {
+    setState(() {
+      indexList[index].isSelected = !indexList[index].isSelected;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    for (var i = 0; i < 50; i++) {
-      indexList.add(Element(isSelected: false));
-    }
-
-    return new Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Title"),
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 15.0),
-            child: Center(child: Text("$selectedCount")),
-          ),
-        ],
+        title: Text("Categorias"),
       ),
-      body: new ListView.builder(
-        itemCount: 50,
+      body: ListView.builder(
+        itemCount: indexList.length,
         itemBuilder: (context, index) {
-          return new CustomWidget(
-            isSelected: indexList[index].isSelected,
+          return CustomWidget(
             index: index,
+            isSelected: indexList[index].isSelected,
             longPressEnabled: longPressFlag,
             callback: () {
               onElementSelected(index);
-              if (indexList.contains(index)) {
-                indexList.remove(index);
-              } else {
-                indexList.add(Element());
-              }
               longPress();
             },
           );
@@ -58,40 +48,30 @@ class _MultiSelectionState extends State<MultiSelection> {
       ),
     );
   }
-
-  onElementSelected(int index) {
-    setState(() {
-      if (indexList[index].isSelected)
-        selectedCount--;
-      else
-        selectedCount++;
-      indexList[index].isSelected = !indexList[index].isSelected;
-    });
-  }
 }
 
 class CustomWidget extends StatefulWidget {
   final int index;
+  final bool isSelected;
   final bool longPressEnabled;
   final VoidCallback callback;
-  final bool isSelected;
 
   const CustomWidget(
       {Key key,
       this.index,
+      this.isSelected,
       this.longPressEnabled,
-      this.callback,
-      this.isSelected})
+      this.callback})
       : super(key: key);
 
   @override
-  _CustomWidgetState createState() => new _CustomWidgetState();
+  _CustomWidgetState createState() => _CustomWidgetState();
 }
 
 class _CustomWidgetState extends State<CustomWidget> {
   @override
   Widget build(BuildContext context) {
-    return new GestureDetector(
+    return GestureDetector(
       onLongPress: () {
         widget.callback();
       },
@@ -100,24 +80,23 @@ class _CustomWidgetState extends State<CustomWidget> {
           widget.callback();
         }
       },
-      child: new Container(
-        margin: new EdgeInsets.all(5.0),
-        child: new ListTile(
-          title: new Text("Title ${widget.index}"),
-          subtitle: new Text("Description ${widget.index}"),
+      child: Container(
+        margin: EdgeInsets.all(5.0),
+        child: ListTile(
+          title: Text("Title ${widget.index}"),
+          subtitle: Text("Description ${widget.index}"),
         ),
         decoration: widget.isSelected
-            ? new BoxDecoration(
-                color: Colors.black38,
-                border: new Border.all(color: Colors.black))
-            : new BoxDecoration(),
+            ? BoxDecoration(
+                color: Colors.black38, border: Border.all(color: Colors.black))
+            : BoxDecoration(),
       ),
     );
   }
 }
 
 class Element {
-  Element({this.isSelected});
-
   bool isSelected;
+
+  Element({this.isSelected});
 }
